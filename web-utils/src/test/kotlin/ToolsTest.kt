a1a574fceba7d0d26ee5dc15ac925c8709940102
Stanislav Karakovsky <skarakovski@icerockdev.com>
2020-02-26 11:25:24 +0700

    Fix tests, add comments about usage

--- web-utils/src/test/kotlin/ToolsTest.kt
+++ web-utils/src/test/kotlin/ToolsTest.kt
@@ -8,8 +8,7 @@ import com.icerockdev.api.Request
 import com.icerockdev.exception.ErrorDetail
 import com.icerockdev.exception.UserException
 import com.icerockdev.i18n.I18N
-import com.icerockdev.validation.DateFormat
-import com.icerockdev.validation.InIntArray
+import com.icerockdev.validation.*
 import org.junit.Assert.assertArrayEquals
 import org.junit.Test
 import java.util.*
@@ -18,10 +17,25 @@ import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
+enum class Status(val value: Int) : IAvailableIntByEnum {
+    STATUS_ACTIVE(30),
+    STATUS_BLOCKED(40);
+
+    override fun getAvailableIntList(listName: String): List<Int> {
+        return when (listName) {
+            "all" -> arrayListOf(STATUS_ACTIVE.value, STATUS_BLOCKED.value)
+            else -> {
+                emptyList()
+            }
+        }
+    }
+}
+
 class ToolsTest {
 
     private val mapper: ObjectMapper = ObjectMapper()
 
+    @FieldMatch(message = "The password fields must match",first = "password", second = "passwordRepeat")
     class TestClass(
         @field:NotNull(message = "Name is required")
         var name: String? = "Mike",
@@ -33,11 +47,17 @@ class ToolsTest {
         @field:Min(2)
         @field:InIntArray(value = [10, 20], message = "Should it be 10 or 20")
         var age: Int = 10,
+        @field:InIntListByEnum(IAvailableIntByEnum = Status::class, message = "Should it be 30 or 40")
+        var status: Int = 30,
         @field:Email(message = "Invalid email")
         var email: String = "test@test.er",
         @field:DateFormat(message = "Date must be in YYYY-MM-DD format", pattern = "YYYY-MM-DD")
         @field:NotNull(message = "Date is required")
-        var date: String = "2020-02-25"
+        var date: String = "2020-02-25",
+        @field:NotNull(message = "Password is required")
+        var password: String = "123456",
+        @field:NotNull(message = "Password repeat is required")
+        var passwordRepeat: String = "123456"
     ) : Request()
 
     @Test
@@ -58,12 +78,14 @@ class ToolsTest {
 
         testObj.name = null
         testObj.age = 21
+        testObj.status = 10
         testObj.email = "efwfwefewfwe"
         testObj.date = "Invalid date"
+        testObj.passwordRepeat = "123457"
 
         val errors = testObj.validate()
 
-        assertEquals(5, errors.size)
+        assertEquals(7, errors.size)
         assertFalse(testObj.isValid())
     }
 
@@ -74,25 +96,29 @@ class ToolsTest {
 
         testObj.name = null
         testObj.age = 21
+        testObj.status = 10
         testObj.email = "efwfwefewfwe"
         testObj.date = "Invalid date"
+        testObj.passwordRepeat = "123457"
 
         val errorsResponse = ErrorResponse(testObj.validate())
         errorsResponse.timestamp = 1566554901677
 
         val expectedErrors = arrayOf(
             ErrorDetail(message="Should it be 10 or 20", code=0),
+            ErrorDetail(message="Should it be 30 or 40", code=0),
             ErrorDetail(message="Date must be in YYYY-MM-DD format", code=0),
             ErrorDetail(message="Name is required", code=0),
             ErrorDetail(message="Invalid email", code=0),
-            ErrorDetail(message="должно быть меньше или равно 14", code=0)
+            ErrorDetail(message="должно быть меньше или равно 14", code=0),
+            ErrorDetail(message="The password fields must match", code=0)
         ).sortedArrayWith(compareBy {it.message})
 
         val actualErrors = errorsResponse.dataList.map {
                 error -> error as ErrorDetail
         }.toTypedArray().sortedArrayWith(compareBy {it.message})
 
-        assertEquals(5, errorsResponse.dataList.size)
+        assertEquals(7, errorsResponse.dataList.size)
         assertEquals(422, errorsResponse.status)
         assertEquals("Validation Error", errorsResponse.message)
         assertEquals(false, errorsResponse.isSuccess)
