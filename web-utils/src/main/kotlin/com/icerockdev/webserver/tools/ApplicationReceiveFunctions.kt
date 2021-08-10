94ea70ffd6013533e71b6c3ecae1d50b9dfa9b19
Oleg Yakovlev <oyakovlev@icerockdev.com>
2021-08-10 15:23:11 +0700

    fixed receiveRequest function

--- web-utils/src/main/kotlin/com/icerockdev/webserver/tools/ApplicationReceiveFunctions.kt
+++ web-utils/src/main/kotlin/com/icerockdev/webserver/tools/ApplicationReceiveFunctions.kt
@@ -10,18 +10,9 @@ import io.ktor.application.ApplicationCall
 import io.ktor.request.receive
 import kotlin.reflect.KClass
 
-suspend inline fun <reified T : Any> ApplicationCall.receiveRequest(): T {
-    return try {
-        receive()
-    } catch (e: MissingKotlinParameterException) {
-        throw BadRequestException(e)
-    } catch (e: Throwable) {
-        application.environment.log.error(e.localizedMessage, e)
-        throw BadRequestException(message = "Invalid request body")
-    }
-}
+suspend inline fun <reified T : Any> ApplicationCall.receiveRequest(): T = receiveRequest(T::class)
 
-suspend inline fun <reified T : Any> ApplicationCall.receiveRequest(type: KClass<T>): T {
+suspend fun <T : Any> ApplicationCall.receiveRequest(type: KClass<T>): T {
     return try {
         receive(type)
     } catch (e: MissingKotlinParameterException) {
